ff3a1b16068bffd2bded7bf67082ce3b3c83470f
kimleehong <klh1523@naver.com>
2023-08-01 15:52:33 +0900

    first

--- src/main/java/com/example/ArtGallery/article/comment/CommentController.java
+++ src/main/java/com/example/ArtGallery/article/comment/CommentController.java
@@ -80,16 +80,28 @@ public class CommentController {
     public String commentModify(@Valid CommentForm commentForm, BindingResult bindingResult,
                                 @PathVariable("commentId") Integer commentId, @PathVariable("postId") int postId) {
         if (bindingResult.hasErrors()) {
-            return "comment_form"; // 유효성 검사 오류가 있을 경우, 같은 폼 뷰를 다시 보여주어 에러 메시지를 표시합니다.
+            return "comment_form"; // 유효성 검사 오류가 있을 경우, 같은 폼 뷰를 다시 보여줍니다.
         }
 
+        // 댓글 수정 전, 원래 댓글 정보를 가져옵니다.
         CommentEntity commentEntity = this.commentService.getComment(commentId);
+
+        // 댓글 수정 로직 처리
+        this.commentService.modify(commentEntity, commentForm.getContent());
 
         PostEntity post = this.postService.getPostById(postId);
 
-        this.commentService.modify(commentEntity, commentForm.getContent());
+        String target = "redirect:/article/details/" + post.getUserEntity().getNickname() + "/" + post.getId();
 
-        return "redirect:/article/details/" + post.getUserEntity().getNickname() + "/" + post.getId() + "#comment_" + commentId;
+        if (commentEntity.getDepth() > 0) {
+            // 답글일 경우, 첫 번째 URL로 리다이렉트합니다.
+             target +=  "#reply_" + commentId;
+        } else {
+            // 일반 댓글일 경우, 두 번째 URL로 리다이렉트합니다.
+            target +=  "#comment_" + commentId;
+        }
+
+        return target;
     }
 
 }
