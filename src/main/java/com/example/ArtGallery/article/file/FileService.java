40b0fa4be60b716ea2ac29bea18aa695a8b28f42
hashin0322 <hashin0322@gmail.com>
2023-08-01 15:55:09 +0900

    카테고리별 정렬

--- src/main/java/com/example/ArtGallery/article/file/FileService.java
+++ src/main/java/com/example/ArtGallery/article/file/FileService.java
@@ -72,7 +72,7 @@ public class FileService {
         FileEntity fileEntity = new FileEntity();
         fileEntity.setUuid(uuid);
         fileEntity.setFileName("fileName.jpg");
-        fileEntity.setFilePath("C:\\IT\\DB\\" + uuid + "_fileName.jpg");
+        fileEntity.setFilePath("C:\\Temp\\upload\\" + uuid + "_fileName.jpg");
         return fileEntity;
     }
 }
